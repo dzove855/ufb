45c05089b578eda9e30823c340d77170743bf362
dzove855 <dzove855@gmail.com>
2022-03-25 11:05:55 +0100

    Add file attr

--- bin/ufb.sh
+++ bin/ufb.sh
@@ -1,11 +1,8 @@
 #!/usr/local/bin/bash
 
-empty()         [[ -z $1 ]]
-fileExist()     [[ -f $1 ]]
-dirExist()      [[ -d $1 ]]
 inArray(){ 
     local -n array="$1"
-    empty "$2" && die "inArray need two arguments: ${BASH_SOURCE[1]}:${BASH_LINENO[0]}"
+    [[ -z "$2" ]] && die "inArray need two arguments: ${BASH_SOURCE[1]}:${BASH_LINENO[0]}"
     [[ "${array[@]}" == *" $2 "* ]]
 }
 
@@ -17,8 +14,10 @@ die(){
 # load loadable builtins
 declare -a RUNTIME_LOAD
 load(){
-    enable -f "${BASH_LOADABLE_PATH:-/usr/local/lib/bash}/$1" "$1" &>/dev/null || return 1
-    RUNTIME_LOAD+=("${BASH_LOADABLE_PATH:-/usr/local/lib/bash}/$1")
+    inArray RUNTIME_LOAD "${BASH_LOADABLE_PATH:-/usr/local/lib/bash}/$1" || {
+        enable -f "${BASH_LOADABLE_PATH:-/usr/local/lib/bash}/$1" "$1" &>/dev/null || return 1
+        RUNTIME_LOAD+=("${BASH_LOADABLE_PATH:-/usr/local/lib/bash}/$1")
+    }
 }
 
 # declare a path of all imported class, so we can always check if an class is imported
@@ -28,13 +27,15 @@ import(){
     local path
 
     # look for package file inside directory
-    empty "$1" && die "Unusual use of import ${BASH_SOURCE[1]}:${BASH_LINENO[0]}"
+    [[ -z "$1" ]] && die "Unusual use of import ${BASH_SOURCE[1]}:${BASH_LINENO[0]}"
     IFS=":" read -a importPath <<<"${IMPORT_PATH:-/usr/share/ufb}"
 
     for path in "${importPath}"; do
-        fileExist "${path%/}/$1.shm" && {
-            RUNTIME_IMPORT+=("${path%/}/$1.shm")
-            source "${path%/}/$1.shm"
+        [[ -f "${path%/}/$1.shm" ]] && {
+            inArray RUNTIME_IMPORT "${path%/}/$1.shm" || {
+                RUNTIME_IMPORT+=("${path%/}/$1.shm")
+                source "${path%/}/$1.shm"
+            }
             return
         }
     done
--- example/testCore.sh
+++ example/testCore.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/ufb.sh
 
 import core/main
+import core/io
 
 declare fuu
 Core::Mktemp -v fuu -d
@@ -8,4 +9,12 @@ Core::Mktemp -v fuu -d
 declare -p fuu
 
 fuu2=(a b c)
-inArray fuu2 
+
+
+declare -A puf
+Core::Io::Attr -v puf /home/dzove855/apps.csv
+declare -p puf
+
+declare -A puf2
+Core::Io::Attr::stat puf2 /home/dzove855/apps.csv
+declare -p puf2
